dbf3e01f2492bf9a8b7946f20626a3cc65fd1fc1
kiver <kiver@ykw-PC>
2020-03-30 15:29:44 +0800

    第一次提交

---
+++ zhzx-admin/src/main/java/com/zhzx/web/controller/system/SysRegisterController.java
@@ -0,0 +1,47 @@
+package com.zhzx.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zhzx.common.core.controller.BaseController;
+import com.zhzx.common.core.domain.AjaxResult;
+import com.zhzx.framework.shiro.service.SysRegisterService;
+import com.zhzx.system.domain.SysUser;
+import com.zhzx.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@Controller
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @GetMapping("/register")
+    public String register()
+    {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    @ResponseBody
+    public AjaxResult ajaxRegister(SysUser user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ zhzx-framework/src/main/java/com/zhzx/framework/shiro/service/SysShiroService.java
@@ -0,0 +1,62 @@
+package com.zhzx.framework.shiro.service;
+
+import java.io.Serializable;
+import org.apache.shiro.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.zhzx.common.utils.StringUtils;
+import com.zhzx.system.domain.SysUserOnline;
+import com.zhzx.system.service.ISysUserOnlineService;
+import com.zhzx.framework.shiro.session.OnlineSession;
+
+/**
+ * 会话db操作处理
+ * 
+ * @author ruoyi
+ */
+@Component
+public class SysShiroService
+{
+    @Autowired
+    private ISysUserOnlineService onlineService;
+
+    /**
+     * 删除会话
+     *
+     * @param onlineSession 会话信息
+     */
+    public void deleteSession(OnlineSession onlineSession)
+    {
+        onlineService.deleteOnlineById(String.valueOf(onlineSession.getId()));
+    }
+
+    /**
+     * 获取会话信息
+     *
+     * @param sessionId
+     * @return
+     */
+    public Session getSession(Serializable sessionId)
+    {
+        SysUserOnline userOnline = onlineService.selectOnlineById(String.valueOf(sessionId));
+        return StringUtils.isNull(userOnline) ? null : createSession(userOnline);
+    }
+
+    public Session createSession(SysUserOnline userOnline)
+    {
+        OnlineSession onlineSession = new OnlineSession();
+        if (StringUtils.isNotNull(userOnline))
+        {
+            onlineSession.setId(userOnline.getSessionId());
+            onlineSession.setHost(userOnline.getIpaddr());
+            onlineSession.setBrowser(userOnline.getBrowser());
+            onlineSession.setOs(userOnline.getOs());
+            onlineSession.setDeptName(userOnline.getDeptName());
+            onlineSession.setLoginName(userOnline.getLoginName());
+            onlineSession.setStartTimestamp(userOnline.getStartTimestamp());
+            onlineSession.setLastAccessTime(userOnline.getLastAccessTime());
+            onlineSession.setTimeout(userOnline.getExpireTime());
+        }
+        return onlineSession;
+    }
+}
